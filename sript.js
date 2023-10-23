fc9a2dacdcf2e2c8ddaf9d786bfc1ed4acbb95b2
Suvhronil Ghosal <146948850+CoderSuvhronil@users.noreply.github.com>
2023-10-23 13:13:40 +0530

    Delete sript.js

--- sript.js
+++
@@ -1,8 +0,0 @@
-function filterNames(array) {
-    const names = ["Alan", "Mark", "Karen", "Elaine", "Nora", "Alice", "Dora"];
-    return array.filter(name => names.includes(name));
-   }
-   
-   const inputArray = ["Alan", "Mark", "Karen", "Elaine", "Nora", "Alan", "Gary", "Alice", "Dora"];
-   
-   console.log(filterNames(inputArray));
